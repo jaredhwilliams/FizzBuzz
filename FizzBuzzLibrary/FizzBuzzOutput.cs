94af9d7996aff850c916bbbaa5c359230e0a9111
Jared Williams <28831807+jaredhwilliams@users.noreply.github.com>
2017-09-08 19:00:47 -0500

    Making a change to confirm AppVeyor automatically kicks off.

--- FizzBuzzLibrary/FizzBuzzOutput.cs
+++ FizzBuzzLibrary/FizzBuzzOutput.cs
@@ -3,17 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-/// <summary>
-/// Allow customer to pass in own set of num/word pairs. As many
-/// as they would like.
-/// Unit test for 15 and other relevant use cases.
-/// Use NUnit as the framework for testing.
-/// Extra credit: add a build script to build solution and project.
-/// any standard build script to compile, run tests, etc.
-/// setup git source control in a fashion that would allow the team
-/// to work in a CI manner.
-/// Put on github when done and send Rayne the URL.
-/// </summary>
+
 namespace FizzBuzz.Library
 {
     public class FizzBuzzOutput
